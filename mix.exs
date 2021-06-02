d20c383d20a407f4c693174f3bf0c43a20210584
Donald Merand <dmerand@explo.org>
2021-06-02 15:58:09 -0400

    chore: update version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExploComm.Mixfile do
   def project do
     [
       app: :explo_comm,
-      version: "2.1.1",
+      version: "2.1.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
